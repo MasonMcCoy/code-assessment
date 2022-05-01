041e2ef22c02b84df11cde5fb2cc2d471ad66505
Mason McCoy <mmasonmccoy@gmail.com>
2022-04-30 22:30:35 -0500

    All functionality has been added.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,28 @@
+// TO-DO:
+// Only one click allowed! Spam clicks break the whole thing
+
 // DOM variables
+// var mainEl = document.querySelector("main");
+var hiScores = document.getElementById("high-scores");
 var quizArea = document.getElementById("quiz-area");
 var timerEl = document.getElementById("timer-text");
 var timerCount = document.getElementById("timer-count");
+var timeLeft = 60;
 var startBttn = document.getElementById("start-button");
 var question = document.getElementById("prompt-area");
 var currentQuestion;
 var quizScore = 0;
+var answersGiven = 0;
 
 // Array of assessment question objects
-var assessQuestions = [{
+var quizQuestions = [{
     prompt: "Which of the following is NOT a JavaScript data type?",
     choices: ["Array", "Object", "Dictionary", "String"],
     answer: "Dictionary"
 }, {
     prompt: "Which of the following is a method of the Web API?",
     choices: [".split()", ".innerHTML", ".createElement()", ".length"],
-    answer: "createElement()"
+    answer: ".createElement()"
 }, {
     prompt: "Which character creates an Object?",
     choices: ["*", "{", "-", "="],
@@ -54,11 +61,12 @@ var assessQuestions = [{
 
 // Randomly selects from array of quiz questions
 function getQuestion() {
-    for (var i = 0; i < assessQuestions.length; i++) {
-        var randomIdx = Math.floor(Math.random() * assessQuestions.length);
-        
+    for (var i = 0; i < quizQuestions.length; i++) {
+        var randomIdx = Math.floor(Math.random() * (quizQuestions.length - 1));
+        // console.log(randomIdx);
+
         // Removes question from the array to prevent duplicates
-        selectedQuestion = assessQuestions.pop(randomIdx)
+        selectedQuestion = quizQuestions.splice(randomIdx, 1);
 
         // Return question object
         return selectedQuestion
@@ -66,27 +74,26 @@ function getQuestion() {
 }
 
 // Adds form to assessment area with question prompt and choices
-function renderForm() {
+function QuestionForm() {
     currentQuestion = getQuestion();
 
     console.log(currentQuestion);
     
-    question.innerText = currentQuestion.prompt;
+    question.innerText = currentQuestion[0].prompt;
 
     var choiceForm = document.createElement("form");
 
-    // Might not need this
     choiceForm.setAttribute("id", "choice-form");
 
     // Creates choices
-    for (var i = 0; i < currentQuestion.choices.length; i++) {
+    for (var i = 0; i < currentQuestion[0].choices.length; i++) {
         var choiceRow = document.createElement("div");
         var choiceCB = document.createElement("input");
         var choiceP = document.createElement("p");
 
         choiceRow.style.display = "flex";
         choiceCB.setAttribute("type", "checkbox");
-        choiceP.innerText = currentQuestion.choices[i];
+        choiceP.innerText = currentQuestion[0].choices[i];
 
         choiceRow.appendChild(choiceCB);
         choiceRow.appendChild(choiceP);
@@ -99,14 +106,14 @@ function renderForm() {
 
 // Adds to total score if select choice is correct, removes times if incorrect
 function checkChoice(choice) {
-    if (choice === currentQuestion.answer) {
+    answersGiven += 1;
+    console.log(answersGiven);
+
+    if (choice === currentQuestion[0].answer) {
         quizScore += 1;
-        console.log('Correct!');
-        console.log(quizScore);
     } else {
-        console.log('Incorrect!');
-        console.log(quizScore);
-        // TO-DO: Remove time from counter
+        // Remove time from counter
+        timeLeft -= 5;
     }
 }
 
@@ -114,9 +121,7 @@ function checkChoice(choice) {
 function startQuiz() {
     startBttn.style.display = "none";
 
-    renderForm();
-
-    var timeLeft = 60;
+    QuestionForm();
 
     var quizTime = setInterval(function() {
         if (timeLeft > 0) {
@@ -127,14 +132,13 @@ function startQuiz() {
             clearInterval(quizTime);
             timerEl.innerText = 'Time is up!';
             clearQuestion()
-            // TO-DO: Initials form
+            recordScore()
 
         }
-        if (!assessQuestions.length && timeLeft > 0) {
+        if (answersGiven === 10 && timeLeft > 0) {
             clearInterval(quizTime);
             console.log("You Win!");
-            clearQuestion()
-            // TO-DO: Initials form
+            setTimeout(() => {recordScore();}, 500);
         }
         
     }, 1000)
@@ -143,19 +147,34 @@ function startQuiz() {
 // Clears quiz area
 function clearQuestion() {
     question.innerText = "";
-    document.getElementById("choice-form").remove();
+    if (document.getElementById("choice-form")) {
+        document.getElementById("choice-form").remove();
+    }
 }
 
 // Reports final score, recrods user initals
-function scoreAlert() {
-    alert("")
+function recordScore() {
+    var scoreForm = document.createElement("form");
+    var scoreP = document.createElement("p");
+    var initialsLabel = document.createElement("label");
+    var initialsInput = document.createElement("input");
+    var scoreBtn = document.createElement("button");
+
+    scoreP.innerText = "Your final score is " + quizScore;
+    initialsLabel.innerText = "Initials";
+    initialsInput.setAttribute("id", "initials-field");
+    scoreBtn.innerText = "Submit";
+    scoreBtn.setAttribute("id", "score-submit");
+
+    scoreForm.appendChild(scoreP);
+    scoreForm.appendChild(initialsLabel);
+    scoreForm.appendChild(initialsInput);
+    scoreForm.appendChild(scoreBtn);
+
+    question.appendChild(scoreForm);    
 }
 
-
-// Begins quiz
-startBttn.addEventListener("click", startQuiz);
-
-quizArea.addEventListener("click", function(event) {
+function clickChoice(event) {
     var userChoice = event.target;
 
     if (userChoice.tagName != "INPUT") {
@@ -164,12 +183,54 @@ quizArea.addEventListener("click", function(event) {
 
     // Checks to see if selected choice matches correct answer
     checkChoice(userChoice.parentElement.innerText);
-
-    clearQuestion();
-
-    if (!assessQuestions.length) {
+    
+    // Brief delay so user can see the result of their input
+    setTimeout(() => {clearQuestion();}, 500);
+    
+    // Handles the array being empty without error, removes event listener for future events
+    if (answersGiven >= 10) {
+        quizArea.removeEventListener("click", clickChoice);
         return
     }
-    
-    renderForm();
+
+    // Brief delay to match the above delay
+    setTimeout(() => {QuestionForm();}, 500);
+}
+
+// Begins quiz
+startBttn.addEventListener("click", startQuiz);
+
+// Event delegation for all question choices
+quizArea.addEventListener("click", clickChoice);
+
+// Accesses local storage
+hiScores.addEventListener("click", function() {
+    startBttn.style.display = "none";
+    clearQuestion();
+
+    var scoreUl = document.createElement("ul");
+    scoreUl.innerText = "High Scores";
+
+    for (var i = 0; i < localStorage.length; i++) {
+        initKey = localStorage.key(i)
+        initVal = localStorage.getItem(localStorage.key(i));
+        
+        var scoreLi = document.createElement("li");
+        
+        scoreLi.innerText = initKey + " has a score of " + initVal;
+        scoreUl.appendChild(scoreLi);
+    }
+
+    quizArea.appendChild(scoreUl);
+})
+
+// Records user score in local storage
+question.addEventListener("click", function(event) {
+    if (event.target && event.target.id === "score-submit") {
+        var initials = document.getElementById("initials-field").value;
+
+        if (initials.length <= 3 && initials.length >= 2) {
+            localStorage.setItem(initials.toUpperCase(), quizScore);
+        }
+    }
 })
